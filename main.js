90346f575254bd724ab3f316168be7b32f9dea71
scrof90 <asknyshev@gmail.com>
2021-08-23 01:23:11 +0300

    Add gameplay functions

--- main.js
+++ main.js
@@ -1,21 +1,65 @@
-// computerPlay() returns 'Rock', 'Paper' or 'Scissors' with 33%
+// computerPlay() returns either 'Rock', 'Paper' or 'Scissors' with 33%
 // probability each
 function computerPlay() {
-  let computerSelection = Math.random();
+  let computer = Math.random();
 
-  if (computerSelection < 0.33) {
-    computerSelection = "Rock";
-  } else if (computerSelection > 0.66) {
-    computerSelection = "Paper";
+  computer = computer < 0.33 ? "Rock" : computer > 0.66 ? "Paper" : "Scissors";
+
+  return computer;
+}
+
+// playRound() returns the result of a round of "Rock, Paper, Scissors"
+function playRound(playerSelection, computerSelection) {
+  const player = capitalizeFirstLetter(playerSelection);
+  const computer = computerSelection;
+
+  let result;
+
+  if (player == computer) {
+    result = `It's a draw! You both chose ${player}`;
+  } else if (
+    (player == "Rock" && computer == "Paper") ||
+    (player == "Paper" && computer == "Rock") ||
+    (player == "Scissors" && computer == "Paper")
+  ) {
+    result = `You win! ${player} beats ${computer}`;
   } else {
-    computerSelection = "Scissors";
+    result = `You lose! ${computer} beats ${player}`;
   }
 
-  return computerSelection;
+  return result;
 }
 
-// playRound(playerSelection, computerSelection) returns the result of a
-// round of "Rock, Paper, Scissors" between player and computer
-function playRound(playerSelection, computerSelection) {
-  return;
+function game() {
+  let wins = 0;
+  let losses = 0;
+
+  for (round = 1; round <= 5; round++) {
+    console.log(`Round ${round} of 5! Fight!`);
+
+    playerSelection = prompt('Enter "rock", "paper" or "scissors": ');
+    const computerSelection = computerPlay();
+    const roundResult = playRound(playerSelection, computerSelection);
+
+    if (roundResult.includes("win")) {
+      wins++;
+    } else {
+      losses++;
+    }
+
+    console.log(roundResult);
+  }
+
+  const gameResult =
+    wins > losses
+      ? `Congratulations! You win with the score of ${wins} - ${losses}`
+      : `Too bad! You lose with the score of ${wins} - ${losses}`;
+
+  console.log(gameResult);
 }
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+game();
