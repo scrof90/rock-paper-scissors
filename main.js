b6592d5589b6d4284af9449e0df21bedfa642088
scrof90 <asknyshev@gmail.com>
2021-08-22 05:46:51 +0300

    Add computerPlay() function to main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// computerPlay() returns 'Rock', 'Paper' or 'Scissors' with 33%
+// probability each
+function computerPlay() {
+  let computerSelection = Math.random();
+
+  if (computerSelection < 0.33) {
+    computerSelection = "Rock";
+  } else if (computerSelection > 0.66) {
+    computerSelection = "Paper";
+  } else {
+    computerSelection = "Scissors";
+  }
+
+  return computerSelection;
+}
+
+// playRound(playerSelection, computerSelection) returns the result of a
+// round of "Rock, Paper, Scissors" between player and computer
+function playRound(playerSelection, computerSelection) {
+  return;
+}
